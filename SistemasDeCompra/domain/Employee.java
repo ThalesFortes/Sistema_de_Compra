a6d495f48a630cf079c182321c2251fce636b377
Thales Fortes <105610872+ThalesFortes@users.noreply.github.com>
2026-08-04 10:14:55 -0300

    Delete SistemasDeCompra directory

--- SistemasDeCompra/domain/Employee.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.maratonajava.javacore.SistemasDeCompra.domain;
-
-public class Employee extends Person {
-    public Employee(int id, String name, int age, boolean employee){
-        super(id, name, age);
-    }
-}
